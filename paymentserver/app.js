f69846bb943bcda193fc2f2c20dbbdc1dcdb5bbc
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-12 16:50:47 +0000

    use token to make stripe payment

--- paymentserver/app.js
+++ paymentserver/app.js
@@ -1,16 +1,31 @@
 var http = require('http');
 
-var port = process.env.PORT || 2000
+var stripe = require('stripe')("sk_test_1vv56eBruuqP9YPX5avh1Ko");
+
+var port = process.env.PORT || 2000;
+
+var chargeObj = paymentData => {
+  amount: 20, // paymentData.amount
+  currency: "gbp",
+  source: paymentData.token,
+  description: "example charge" //paymentData.example
+}
 
 var server = http.createServer(function(request, response) {
-  response.setHeader('Access-Control-Allow-Origin', 'https://ribbonmvp.parseapp.com');
+  response.setHeader('Access-Control-Allow-Origin', 'https://ribbonmvp.parseapp.com/pay');
   console.log(request.url);
   if (request.url === '/') {
     response.end('hello, world');
   } else if (request.url === "/pay" && request.method === 'POST') {
     getBody(request, function(body) {
-      console.log(JSON.parse(body));
-      response.end(JSON.stringify(body));
+      var paymentData = JSON.parse(body).token
+      stripe.charges.create(chargeObj(paymentData), function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+          response.end('error!, payment not accepted', err);
+        } else {
+          response.end('payment success');
+        }
+      });
     });
     // response.end("welcome to pay endpoint");
   } else {
