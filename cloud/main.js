993cf55ea6df81defe8f680c2ee97324dee1da11
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-13 11:11:22 +0000

    fixed up merge

--- cloud/main.js
+++ cloud/main.js
@@ -1,35 +1,7 @@
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-<<<<<<< HEAD
-// console.log("--------->>>>>>>>>im in main.js");
-// var Stripe = require('cloud/node_modules/stripe/lib/stripe.js');
-// Stripe.initialize('sk_test_1vv56eBruuqP9YPX5avhlK8o');
-//
-// Parse.Cloud.define("charge", function(request, response) {
-// //  if (request === "undefined")
-//    console.log("--------->>>>>>>>>in function charge");
-//   //console.log(request);
-//
-//   var stripeToken = request.params.token;
-//   console.log(stripeToken);
-//   var charge = Stripe.charges.create({
-//     amount: 20, // $10 expressed in cents
-//     currency: "gbp",
-//     card: stripeToken // the token id should be sent from the client
-//   }, function(err, charge) {
-//     if (err && err.type === 'StripeCardError') {
-//       console.log("card declined", err);
-//     } else {
-//       console.log("Cloud Success!", charge);
-//     }
-//   });
-// =======
+
 Parse.Cloud.define("emailExists", function(request, response) {
   console.log("cloud code emailExists is running");
-  // var acl = // create with master key
   var User = Parse.Object.extend("User");
-  // var user = new User()
-  // user.setALC(alc);
   var query = new Parse.Query(User);
   query.find({
     useMasterKey:true,
@@ -37,10 +9,6 @@ Parse.Cloud.define("emailExists", function(request, response) {
       var usernames = userData.map(function(user) {
         return user.attributes.username;
       });
-
-      console.log("usernames");
-      console.log(usernames);
-
       response.success(usernames.indexOf(request.params.username) !== -1);
     },
     error: function(error) {
