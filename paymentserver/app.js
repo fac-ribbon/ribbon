192aea97667e7f810a5608b6624740a1f50ef2b1
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-13 14:30:27 +0000

    added cloud code for checkout product cost. added proper payment based on productid

--- paymentserver/app.js
+++ paymentserver/app.js
@@ -1,11 +1,30 @@
 var http = require('http');
 
 var stripe = require('stripe')("sk_test_1vv56eBruuqP9YPX5avhlK8o");
+var Parse = require('parse/node').Parse;
+Parse.initialize("nhFykJoUL5INlggaBWAYf99p7xeu06bOuyhc4iSx", "8xjV4an8FK3OngCErKac7l3OlZntb4w1NtDfNtVZ");
 
-var querystring = require('querystring');
-var url = require('url');
 
-var port = process.env.PORT || 2000;
+var getPrice = function(productId, callback) {
+  Parse.Cloud.run("getItemCost", {productId: "4tcI7tbxrV"}).then(function(result) {
+    callback(JSON.parse(result)[0]);
+  });
+};
+
+var buyItem = function(giftName, callback) {
+  var Buy = Parse.Object.extend('Buy');
+  var buy = new Buy();
+  buy.set("giftName", giftName);
+  // newBuy.set("message", "I want this to arrive at 6th July 2015");
+  buy.save({
+    success: function() {
+      callback(true);
+    },
+    error: function() {
+      callback(false);
+    }
+  });
+}
 
 var chargeObj = paymentData => ({
   amount: paymentData.amount,
@@ -13,6 +32,33 @@ var chargeObj = paymentData => ({
   source: paymentData.stripeToken,
   description: "example charge" //paymentData.example
 });
+
+var makePayment = function(paymentData, callbackError, callbackSuccess) {
+  getPrice(paymentData.productId, function(productData) {
+    paymentData.amount = productData.PricePence;
+    var chargeData = chargeObj(paymentData);
+    stripe.charges.create(chargeData, function(err, charge) {
+      // if (err) console.log(err);
+      if (err) {
+        callbackError();
+        } else {
+          buyItem(productData.gift, function(success) {
+            if (!success) {
+                throw new Error('This should not happen, payment with token '+ chargeData.source +'failed');
+            } else {
+              callbackSuccess();
+            }
+          }
+        );
+      }
+    });
+  });
+}
+
+var querystring = require('querystring');
+var url = require('url');
+
+var port = process.env.PORT || 2000;
 
 var server = http.createServer(function(request, response) {
   var urlData = url.parse(request.url, true);
@@ -28,6 +74,7 @@ var server = http.createServer(function(request, response) {
       paymentData.amount = urlData.search.split('=')[1];
       var charge = chargeObj(paymentData);
       stripe.charges.create(charge, function(err, charge) {
+        if (err) console.log(err);
         if (err && err.type === 'StripeCardError') {
           response.writeHead(302, {'Location': 'https://ribbonmvp.parseapp.com/html/payment-error.html'})
           response.end();
