53777fb9b0706ae8804f8aaaffb700bab11d0391
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-19 11:23:33 +0000

    price now also shows on payment page

--- public/js/renderProducts.js
+++ public/js/renderProducts.js
@@ -3,16 +3,6 @@ var products = (function() {
   var Products = Parse.Object.extend("gifts");
   var query = new Parse.Query(Products);
 
-  var formatPrice = function(pence) {
-    var penceNum = parseInt(pence, 10);
-    var pounds = Math.floor(pence/100);
-    var penceNum = penceNum - pounds*100;
-    penceStr = penceNum.toString();
-    penceStr = penceStr.length === 2 ? penceStr :
-      penceStr.length === 1 ? "0" + penceStr : "00";
-    return "£" + pounds.toString() + "." + penceStr;
-  }
-
   var createProductHtml = function(products) {
     return products.map(function(product, index){
       var attr = product.attributes;
